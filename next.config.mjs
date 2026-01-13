1ab99f9fd8a58f7560357d1473fa3d9d63e391f4
Atik Hassan <atikh01001@gmail.com>
2026-01-13 15:30:43 +0600

    carousel create done

--- next.config.mjs
+++ next.config.mjs
@@ -2,18 +2,6 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
-  images:{
-    remotePatterns:[
-      {
-        hostname: "encrypted-tbn0.gstatic.com",
-        protocol: "https",
-      },
-      {
-        hostname: "media.istockphoto.com",
-        protocol: "https",
-      },
-    ]
-  }
 };
 
 export default nextConfig;
