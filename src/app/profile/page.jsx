d0ce395d2bfafcda08e8d3a684f924b0bdd64973
MasudRiam <masudp.riam@gmail.com>
2026-02-07 23:02:36 +0600

    restructure folder and file better way. solve ssr and isr issue

--- src/app/profile/page.jsx
+++
@@ -1,9 +0,0 @@
-import ProfileClient from "./ProfileClient";
-import { shopApi } from "@/utility/shopApi";
-
-export default async function Page() {
-  // Server-side fetch
-  const UserInfo = await shopApi.getUserInfo();
-
-  return <ProfileClient UserInfo={UserInfo.data} />;
-}
